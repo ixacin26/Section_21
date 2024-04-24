63acb4eba2b54b212cf4901bffd168892f84e362
ixacin26 <ixacin26@gmail.com>
2024-04-24 07:27:35 +0200

    Tennisstüberl-Geistesblitz

--- 21_final-project.py
+++ 21_final-project.py
@@ -10,9 +10,21 @@ character = input("Choose a character from the Pokemon-universe: ")
 
 req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{character}")
 charac = req.json()
+abilities = charac["abilities"]
+print(abilities[0]["ability"]["name"])
 
-print(charac)
 
+print(f"Data-type of charac: {type(charac)}")
+# print(charac)
+
+
+print(f"The name of the chosen character ist {charac["name"].capitalize()}")
+print(f"The height of {charac["name"].capitalize()} is {charac["height"]}")
+print(f"The weight of {charac["name"].capitalize()} is {charac["weight"]}")
+print(f"{charac["name"].capitalize()} has the following abilities: {charac["abilities"]}")
+# print(f"Data-type of Abilities: {type(abilities)}")
+# print(abilities)
+# print(abilities[1])
 
 # try:
 #     print(respone.json())
@@ -22,7 +34,7 @@ print(charac)
 # for i in charac:
 #     print(charac[i].keys())
 
-# print(charac["abilities"])
+
 # for i in charac:
     # print(charac[i].keys())
     # print(charac[i])
